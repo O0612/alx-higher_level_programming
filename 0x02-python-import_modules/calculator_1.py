42637b7efdea055a4ef17beb28e1ca73877bc8fe
O0612 <okezie.nkemakolam@routelinksys.com>
2023-06-09 03:33:55 -0700

    updated

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-
 def add(a, b):
     """My addition function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a + b
     """
@@ -13,33 +13,39 @@ def add(a, b):
 
 
 def sub(a, b):
-    """My subtraction function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
+	"""My subtraction function
+
+	Args:
+		a: first integer
+		b: second integer
+
+	Returns:
+		The return value. a - b
+	"""
+	return (a - b)
 
 
 def mul(a, b):
-    """My multiplication function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
+	"""My multiplication function
+
+	Args:
+		a: first integer
+		b: second integer
+
+	Returns:
+		The return value. a * b
+	"""
+	return (a * b)
 
 
 def div(a, b):
-    """My division function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
+	"""My division function
+
+	Args:
+		a: first integer
+		b: second integer
+
+	Returns:
+		The return value. a / b
+	"""
+	return int(a / b)
